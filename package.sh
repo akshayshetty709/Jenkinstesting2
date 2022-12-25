f58c9959995d97e8571c7c0f2dd61b649720eadf
akshayshetty709 <114096841+akshayshetty709@users.noreply.github.com>
2022-12-25 22:12:22 +0530

    Create package.sh

---
+++ package.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+install()
+{
+        sudo apt install npm -y
+        sudo apt install awscli -y
+        sudo apt install netcat -y
+        sudo curl https://apt.releases.teleport.dev/gpg \
+-o /usr/share/keyrings/teleport-archive-keyring.asc
+source /etc/os-release
+echo "deb [signed-by=/usr/share/keyrings/teleport-archive-keyring.asc] \
+https://apt.releases.teleport.dev/${ID?} ${VERSION_CODENAME?} stable/v11" \ | sudo tee /etc/apt/sources.list.d/teleport.list > /dev/null
+sudo apt-get update -y
+sudo apt-get -y install teleport
+}
+install
