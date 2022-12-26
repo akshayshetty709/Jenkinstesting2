ec051e7ca1de5a3757d3946943b52254c14d6756
akshayshetty709 <114096841+akshayshetty709@users.noreply.github.com>
2022-12-26 17:42:07 +0530

    Update package.sh

--- package.sh
+++ package.sh
@@ -11,6 +11,5 @@ echo "deb [signed-by=/usr/share/keyrings/teleport-archive-keyring.asc] \
 https://apt.releases.teleport.dev/${ID?} ${VERSION_CODENAME?} stable/v11" \ | sudo tee /etc/apt/sources.list.d/teleport.list > /dev/null
 sudo apt update -y
 sudo apt  install teleport -y
-sudo apt install tsh -y
 }
 install
